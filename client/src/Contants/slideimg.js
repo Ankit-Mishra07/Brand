9e8dfce6578d525d3887c9a95bfc90efe4b052e1
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-09 13:18:20 +0530

    creating home cards

--- client/src/Contants/slideimg.js
+++ client/src/Contants/slideimg.js
@@ -2,5 +2,6 @@ export const SlidesImages = [
     "https://media.tenor.com/images/40370d22423aca3436e0334d21f986ce/tenor.gif",
     "https://media.giphy.com/media/3og0IPNcCRz8Tizbd6/giphy.gif",
     "https://live.staticflickr.com/6154/6146440074_4743f86051.jpg",
+    "https://th.bing.com/th/id/OIP.nwAfPs_j3Q7vu_QSBvpB6gHaFg?w=263&h=195&c=7&r=0&o=5&pid=1.7",
     "https://cdn.wallpaper.com/main/legacy/gallery/17054615/03_Retail-Directory_Fashion_Sneakerboy.gif"
 ]
---
+++ client/src/components/HomeCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeCard = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://www.bing.com/th?id=OIP.fhJAdLE4Iq9SuE1y-Cq7YQHaEK&w=165&h=95&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
+                
+            </div>
+        </div>
+    )
+}
+
+export default HomeCard
